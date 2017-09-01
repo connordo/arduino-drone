be76bada8df93ab068e1b95db86031e9c00c4b44
Connor <connorolsen72@gmail.com>
2017-08-31 23:37:38 -0600

    a minor change to the motor test

--- Drone_Motor_Test/Drone_Motor_Test.ino
+++ Drone_Motor_Test/Drone_Motor_Test.ino
@@ -11,12 +11,12 @@ void setup() {
 }
 
 void loop() {
-  //  analogWrite(MOTOR, MAX_SPEED);
-  //  delay(TIME_INTERVAL);
+  analogWrite(MOTOR, MAX_SPEED);
+  delay(TIME_INTERVAL);
   //  analogWrite(MOTOR, HIGH_SPEED);
   //  delay(TIME_INTERVAL);
   //  analogWrite(MOTOR, LOW_SPEED);
   //  delay(TIME_INTERVAL);
-  //  analogWrite(MOTOR, OFF);
-  //  delay(TIME_INTERVAL);
+  analogWrite(MOTOR, OFF);
+  delay(TIME_INTERVAL);
 }
