527d61cb122b8e13594d9c79e7e6076cd35ce9c7
Connor <connorolsen72@gmail.com>
2017-09-11 14:52:33 -0600

    probably just some graphing changes

--- IMU_test/IMU_test.ino
+++ IMU_test/IMU_test.ino
@@ -3,12 +3,12 @@
 #define    MPU9250_ADDRESS            0x68
 #define    MAG_ADDRESS                0x0C
 
-#define    GYRO_FULL_SCALE_250_DPS    0x00
+#define    GYRO_FULL_SCALE_250_DPS    0x00 //degrees per second
 #define    GYRO_FULL_SCALE_500_DPS    0x08
 #define    GYRO_FULL_SCALE_1000_DPS   0x10
 #define    GYRO_FULL_SCALE_2000_DPS   0x18
 
-#define    ACC_FULL_SCALE_2_G        0x00
+#define    ACC_FULL_SCALE_2_G        0x00 //something to do with sample rate
 #define    ACC_FULL_SCALE_4_G        0x08
 #define    ACC_FULL_SCALE_8_G        0x10
 #define    ACC_FULL_SCALE_16_G       0x18
@@ -37,20 +37,19 @@ void I2CwriteByte(uint8_t Address, uint8_t Register, uint8_t Data)
   Wire.endTransmission();
 }
 
-
 void setup() {
   Serial.begin(9600);
   Wire.begin();
 
   // Configure gyroscope range
-  I2CwriteByte(MPU9250_ADDRESS, 27, GYRO_FULL_SCALE_1000_DPS);
+  I2CwriteByte(MPU9250_ADDRESS, 27, GYRO_FULL_SCALE_2000_DPS);
   // Configure accelerometers range
-  I2CwriteByte(MPU9250_ADDRESS, 28, ACC_FULL_SCALE_8_G);
+  I2CwriteByte(MPU9250_ADDRESS, 28, ACC_FULL_SCALE_16_G);
   // Set by pass mode for the magnetometers
   I2CwriteByte(MPU9250_ADDRESS, 0x37, 0x02);
 
   // Request first magnetometer single measurement
-  //  I2CwriteByte(MAG_ADDRESS, 0x0A, 0x01); //I rdon't think I wasnt to do this yet....
+  I2CwriteByte(MAG_ADDRESS, 0x0A, 0x01); //I rdon't think I wasnt to do this yet....
 
   Serial.write("Beginning I2C Read Attempt:\n");
 }
