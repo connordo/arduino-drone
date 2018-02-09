a82372c7efbcfa458b8b66ec77532e67553b8ed8
Connor <connorolsen72@gmail.com>
2018-02-09 10:44:27 -0700

    changed the #include on the comms.ino

--- FlightSW_test/comms.ino
+++ FlightSW_test/comms.ino
@@ -1,8 +1,9 @@
-#include <softserial.h>
+#include <SoftwareSerial.h>
 
 #define TX_D 8 //These are subject to change
 #define RX_D 7 //These are subject to change
 
 int comms_init(){
   SoftwareSerial bluetooth(RX_D, TX_D);
+  bluetooth.begin(9600);
 }
